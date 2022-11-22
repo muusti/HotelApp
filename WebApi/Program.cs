d8668551b663cfc8313cb8de07ba3736bfaea51e
muusti <mustfa.cinaar@gmail.com>
2022-11-22 13:30:08 +0300

    V Final

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,13 +1,60 @@
+using DataAccess.Contexts;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+#region Localization
+List<CultureInfo> cultures = new List<CultureInfo>()
+{
+    new CultureInfo("en-US")
+};
 
-builder.Services.AddControllers();
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    options.DefaultRequestCulture = new RequestCulture(cultures.FirstOrDefault().Name);
+    options.SupportedCultures = cultures;
+    options.SupportedUICultures = cultures;
+});
+#endregion
+
+
+//IoC Container.
+var connectionString = builder.Configuration.GetConnectionString("HotelAppDb");
+builder.Services.AddDbContext<Db>(options => options.UseSqlServer(connectionString));
+
+
+builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+#region CORS (Cross Origin Resource Sharing)
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    builder.AllowAnyOrigin()
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    );
+});
+
+#endregion
+
+
 var app = builder.Build();
+
+#region Localization
+app.UseRequestLocalization(new RequestLocalizationOptions()
+{
+    DefaultRequestCulture = new RequestCulture(cultures.FirstOrDefault().Name),
+    SupportedCultures = cultures,
+    SupportedUICultures = cultures,
+});
+#endregion
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
